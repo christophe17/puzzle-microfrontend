81e3fcabba226043dbd359971b563eebcf9b0a90
Christophe Dufour <dufour.chri@gmail.com>
2021-12-28 12:26:52 +0100

    added others mf - WIP

---
+++ mf-container/config/webpack.dev.js
@@ -0,0 +1,70 @@
+const paths = require('./paths')
+const Dotenv = require('dotenv-webpack')
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const packageJson = require('../package.json')
+// const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+// var webpack = require('webpack')
+
+module.exports = merge(common, {
+  // Set the mode to development or production
+  mode: 'development',
+
+  // Control how source maps are generated
+  devtool: 'inline-source-map',
+
+  output: {
+    publicPath: 'http://localhost:8080/',
+  },
+  
+  // Spin up a server for quick development
+  devServer: {
+    historyApiFallback: true,
+    contentBase: paths.build,
+    open: false,
+    compress: true,
+    hot: false, //@TODO : set to true
+    port: 8080,
+    // host: '0.0.0.0',  // needed inside docker compose
+  },
+
+  module: {
+    rules: [
+      // ... other rules
+      {
+        test: /\.[js]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          // ... other loaders
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              // ... other options
+              plugins: [
+                // ... other plugins
+                // require.resolve('react-refresh/babel'), 
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env.development',
+    }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+        auth: 'auth@http://localhost:8082/remoteEntry.js',
+        dashboard: 'dashboard@http://localhost:8083/remoteEntry.js',
+      },
+      shared: packageJson.dependencies,
+    }),
+    // new webpack.HotModuleReplacementPlugin(),
+    // new ReactRefreshWebpackPlugin(), // Issue : https://github.com/pmmmwh/react-refresh-webpack-plugin/issues/245
+  ].filter(Boolean),
+})
